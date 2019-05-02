10edc2a245094388bce6919d70d175d3caee0261
GiuseppinaC <carannantegiuse@gmail.com>
2019-05-02 16:28:22 -0400

    modified run file

--- run1.sh
+++ run1.sh
@@ -11,5 +11,5 @@ export CUDA_HOME=/usr/local/cuda-9.0
 export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64
 export PATH=/usr/local/cuda-9.0/bin:$PATH
 . /home1/cg19017/.local/share/virtualenvs/Project-uT6U-LQR/bin/activate
-cd /gpfs/gpfs/project1/gr19002-002/giuse/Project
-python train2.py
+cd /gpfs/gpfs/project1/gr19002-002/giuse/ECE_Final_Project
+python train2.py --dataset dataset --model final.model --labelbin mlb.pickle
